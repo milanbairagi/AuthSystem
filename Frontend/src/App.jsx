7a9da35644c3903f1c914aa65c6f7ef890b4eb43
NoName3755 <nmilan626@gmail.com>
2025-09-29 21:53:02 +0545

    feat: add basic routing for home, login, and register pages
    Introduces client-side routing to enable navigation between the home, login, and register views.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,36 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+
+const Home = () => {
+  return (
+    <div>Home Page</div>
+  );
+};
+
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
 
   return (
-    <>
-      <h3 className="text-2xl font-bold">Welcome to the App!</h3>
-    </>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;

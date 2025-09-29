c4e26313ffcd4a43e4bdfd4162438d71d10baa8f
NoName3755 <nmilan626@gmail.com>
2025-09-29 18:41:04 +0545

    feat: add custom user model with role management and admin integration
    - Introduces a custom user model from django `AbstractUser`
    - Add `email` as the unique identifier and role-based functionality
      (admin/user)
    - Implement a custom manager for user and superuser creation with
      validation.
    - Integrates the user model into Django admin with enhanced display,
      filtering, and search capabilities
    - Updates settings for user model and add migrations for the new model

--- Backend/accounts/admin.py
+++ Backend/accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "role", "first_name", "last_name", "is_staff", "is_active")
+    list_filter = ("role", "is_staff", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "role")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ("admin", "Admin"),
+        ("user", "User"),
+    )
+    username = None
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="user")
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()

4b4bee3ab24daee992ba951a3ad401f71f797cd5
NoName3755 <nmilan626@gmail.com>
2025-10-01 18:30:10 +0545

    feat(settings): add SIMPLE_JWT configuration for token lifetime management

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,6 +88,11 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
 }
 
 

cd4a9d61a3e6a7b6cbafcf4f0668135dd4fe1463
NoName3755 <nmilan626@gmail.com>
2025-10-01 13:12:19 +0545

    fix(button): remove unnecessary margin from button components

--- Frontend/src/components/Button.jsx
+++ Frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ const PrimaryButton = ({ text, type, styles, onClick, loading=false, loadingText
 	return (
 		<button
 			type={type}
-			className={`bg-blue-500 text-white px-4 py-2 rounded mt-4 ${loading ? "opacity-70" : ""} ${styles}`}
+			className={`bg-blue-500 text-white px-4 py-2 rounded ${loading ? "opacity-70" : ""} ${styles}`}
 			onClick={onClick}
       disabled={loading}
 		>
@@ -15,7 +15,7 @@ const SecondaryButton = ({ text, type, styles, onClick, loading=false, loadingTe
 	return (
 		<button
 			type={type}
-			className={`bg-gray-500 text-white px-4 py-2 rounded mt-4 ${loading ? "opacity-70" : ""} ${styles}`}
+			className={`bg-gray-500 text-white px-4 py-2 rounded ${loading ? "opacity-70" : ""} ${styles}`}
 			onClick={onClick}
       disabled={loading}
 		>
